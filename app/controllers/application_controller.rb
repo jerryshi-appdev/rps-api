cf9b4de12b2cecd5afc78d569b042f009dec2560
Jerry Shi <jerryshi25@gmail.com>
2019-11-15 18:06:22 +0000

    finished rock paper scissors game

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,61 @@
 class ApplicationController < ActionController::Base
+  def play_rock
+    moves = ["rock", "paper", "scissors"]
+
+    computer_move = moves.sample
+    
+    if computer_move == "rock"
+      outcome = "tied"
+    elsif computer_move == "paper"
+      outcome = "lost"
+    elsif computer_move == "scissors"
+      outcome = "won"
+    end
+
+    full_message = "You played rock. They played " + computer_move + ". You " + outcome + "!"
+
+    render({ :plain =>  full_message })
+  end
+  def play_paper
+    moves = ["rock", "paper", "scissors"]
+
+    computer_move = moves.sample
+    
+    if computer_move == "rock"
+      outcome = "won"
+    elsif computer_move == "paper"
+      outcome = "tied"
+    elsif computer_move == "scissors"
+      outcome = "lost"
+    end
+
+    full_message = "You played paper. They played " + computer_move + ". You " + outcome + "!"
+
+    render({ :plain =>  full_message })
+  end
+  def play_scissors
+    moves = ["rock", "paper", "scissors"]
+
+    computer_move = moves.sample
+    
+    if computer_move == "rock"
+      outcome = "lost"
+    elsif computer_move == "paper"
+      outcome = "won"
+    elsif computer_move == "scissors"
+      outcome = "tied"
+    end
+
+    full_message = "You played scissors. They played " + computer_move + ". You " + outcome + "!"
+
+    render({ :plain =>  full_message })
+  end
+    def homepage
+    date = Date.today.strftime("%A")
+    full_message = "Today is " + date + ".
+Rock beats Scissors, Paper beats Rock, Scissors beats Paper.
+Point your browser at /rock, /paper, or /scissors to play the game."
+
+    render({ :plain =>  full_message })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
+    match("/rock", { :controller => "application", :action => "play_rock", :via => "get" })
 
+    match("/paper", { :controller => "application", :action => "play_paper", :via => "get" })
 
+    match("/scissors", { :controller => "application", :action => "play_scissors", :via => "get" })
+
+    match("/", { :controller => "application", :action => "homepage", :via => "get" })
 
 # ==================================================
   devise_for :admin_users, ActiveAdmin::Devise.config
